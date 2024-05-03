899beb73dc731e40dce2de0b1eb6160d4545ebaa
Anurag Singh Naruka <anuragsinghnaruka@gmail.com>
2024-05-04 00:08:00 +0530

    info change

--- src/main/java/com/HAD/AuthService/Controller/AuthController.java
+++ src/main/java/com/HAD/AuthService/Controller/AuthController.java
@@ -9,8 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/auth")
 public class AuthController {
 
